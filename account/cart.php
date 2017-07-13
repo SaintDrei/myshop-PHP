7873b3068d648199d39c63bc035f4d0a4485a8a7
Andrei Santos <andreimishael.santos@benilde.edu.ph>
2017-07-13 16:30:23 +0800

    Added Cart function
    Cart Function working 70/30

---
+++ account/cart.php
@@ -0,0 +1,99 @@
+<?php 
+$page_title = "My Cart";
+include_once ("../includes/header.php");
+
+    $sql_cart = "SELECT od.detailID, od.productID, p.image, p.name, c.name AS category, p.price, od.quantity, od.amount FROM orderdetails od
+    INNER JOIN products p ON od.productID = p.productID
+    INNER JOIN categories c ON p.catID = c.catID
+    WHERE od.orderNo = 0 AND od.userID = 1";
+    $result_cart = $con->query($sql_cart) or die(mysqli_error($con));
+    $list_cart = "";
+    while($row = mysqli_fetch_array($result_cart))
+    {
+        $did = $row['detailID'];
+        $pid = $row['productID'];
+        $image = $row['image'];
+        $pname = $row['name'];
+        $cat = $row['category'];
+        $price = $row['price'];
+        $qty = $row['quantity'];
+        $amount = $row['amount'];
+        
+        
+        $list_cart .= "<tr>
+        <td><img src='../images/products/$image' width='150' /></td>
+        <td><h3>$pname</h3>
+        <small><em>$cat</em></small>
+        </td>
+        <td>$price</td>
+        
+        
+        <td>
+        <input type='number' class='form-control' min='1' max'1000' value='$qty' required/>
+        </td>
+        <td>P$amount</td>
+        <td>
+        <button class='update btn btn-success btn-xs'>
+        <i class='fa fa-trash-o'></i>
+        </td>
+        <tr>
+        ";
+        
+    }
+
+    $sql_compute = "SELECT SUM(amount) FROM orderdetails WHERE orderNo = 0 AND userID=1";
+$result_compute = $con->query($sql_compute) or die(mysqli_error($con));
+while ($row2 = mysqli_fetch_array($result_compute))
+{
+    $total = $row2[0];
+    $gross = $total * .88;
+    $VAT = $total *.12;
+}
+?>
+
+<div class="col-lg-8">
+    <form class="form-horizontal" method="POST">
+        <table class="table table-hover">
+            <thead>
+                <th colspan="2">Item</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Amount</th>
+                <th></th>
+            
+            </thead>
+            <tbody>
+            <?php echo $list_cart; ?>
+            </tbody>
+            
+        </table>
+    </form>
+</div>
+<div class="col-lg-4">
+		<div class="well">
+			<h3 class="text-center">Order Summary</h3>
+			<table class="table table-hover">
+				<tr>
+					<td>Gross Amount</td>
+					<td align='right'>P<?php echo $gross; ?></td>
+				</tr>
+				<tr>
+					<td>VAT</td>
+					<td align='right'><?php echo $VAT; ?></td>
+				</tr>
+				<tr>
+					<td><b>Total Amount</b></td>
+					<td align='right'><b>P<?php echo $total; ?></b></td>
+				</tr>
+			</table>
+			<hr/>
+			<a href='checkout.php' class='btn btn-success btn-lg btn-block'>
+				<i class='fa fa-money'></i> Checkout
+			</a>
+		</div>
+	</div>
+
+<?php 
+include_once("../includes/footer.php");
+?>
+
